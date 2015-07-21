47b3c35cb8b3ee28f6fe0e8a9a209111e195a326
juniormesquitadandao <juniormesquitadandao@gmail.com>
2015-07-21 10:22:26 -0300

    removendo require "action_controller/metal/responder"

--- lib/jasper-rails.rb
+++ lib/jasper-rails.rb
@@ -23,7 +23,7 @@
 require "jasper-rails/version"
 require "rails"
 require "rjb"
-require "action_controller/metal/responder"
+# require "action_controller/metal/responder"
 
 if Mime::Type.lookup_by_extension("pdf").nil?
   Mime::Type.register "application/pdf", :pdf
