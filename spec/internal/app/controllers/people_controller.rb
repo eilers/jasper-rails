93a5f7f4bb5d1f1326eb224928d645269f0cd26f
Stefan Eilers <stefan.eilers@intelligentmobiles.com>
2019-01-07 15:11:20 +0100

    Update to JasperReports 6.7

--- spec/internal/app/controllers/people_controller.rb
+++ spec/internal/app/controllers/people_controller.rb
@@ -1,33 +1,34 @@
+require "lib/jasper-rails"
 class PeopleController < ActionController::Base
-  respond_to :xml, :pdf
+    respond_to :xml, :pdf
 
-  def index
-    @people = Person.all
+    def index
+        @people = Person.all
 
-    @parameter_defined_in_the_controller = "I'm a parameter. I was defined in the controller"
+        @parameter_defined_in_the_controller = "I'm a parameter. I was defined in the controller"
 
-    respond_with @people
-  end
+        respond_with @people
+    end
 
-  def compile_time_error_report
-    respond_with []
-  end
+    def compile_time_error_report
+        respond_with []
+    end
 
-  def runtime_error_report
-    respond_with []
-  end
+    def runtime_error_report
+        respond_with []
+    end
 
-  def nil_datasource
-    @parameter_defined_in_the_controller = "I'm a parameter. I was defined in the controller"
-    respond_with nil
-  end
+    def nil_datasource
+        @parameter_defined_in_the_controller = "I'm a parameter. I was defined in the controller"
+        respond_with nil
+    end
 
-  def java_parameter
-    title = (Rjb::import 'java.lang.String').new("The Beatles")
-    @parameter_that_is_a_java_class = (Rjb::import 'java.util.HashMap').new
-    @parameter_that_is_a_java_class.put("title", title)
+    def java_parameter
+        title = (Rjb::import 'java.lang.String').new("The Beatles")
+        @parameter_that_is_a_java_class = (Rjb::import 'java.util.HashMap').new
+        @parameter_that_is_a_java_class.put("title", title)
 
-    respond_with nil
-  end
+        respond_with nil
+    end
 
 end
